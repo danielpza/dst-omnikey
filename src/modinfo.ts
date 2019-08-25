66fa8752560575868c05d83d1cee72e913c62720
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-08-25 01:14:02 -0400

    fix hammer

--- src/modinfo.ts
+++ src/modinfo.ts
@@ -27,7 +27,7 @@ var configuration_options = [
   Keybind("AXE", "1", "Axe Key"),
   Keybind("PICKAXE", "2", "Pickaxe Key"),
   Keybind("SHOVEL", "3", "Shovel Key"),
-  Keybind("SHOVEL", "4", "Hammer Key"),
+  Keybind("HAMMER", "4", "Hammer Key"),
   Keybind("SCYTHE", "5", "Scythe Key"),
   Keybind("ARMOR", "c", "Armor"),
   Keybind("HELMET", "v", "Helmet")
