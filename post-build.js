a8f5a4773d309ce6488e0d8bf7aed9b35f73c017
Daniel Perez Alvarez <danielpza@protonmail.com>
2019-12-16 22:58:38 -0500

    refact: add scripts helpers

--- post-build.js
+++ post-build.js
@@ -1,22 +1,32 @@
 const { readFileSync, writeFileSync } = require("fs");
 const { resolve } = require("path");
 
-const modinfo = resolve(__dirname, "./lib/modinfo.lua");
-const modmain = resolve(__dirname, "./lib/modmain.lua");
 const { version, description, homepage } = require("./package.json");
 
-writeFileSync(
-  modinfo,
-  readFileSync(modinfo, "utf8")
-    .toString()
+function updateFile(file, updater) {
+  writeFileSync(file, updater(readFileSync(file, "utf8").toString()));
+}
+
+updateFile(resolve(__dirname, "./lib/modinfo.lua"), content =>
+  content
     .replace("$VERSION", version)
     .replace("$DESCRIPTION", description)
     .replace("$HOMEPAGE", homepage)
 );
 
-writeFileSync(
-  modmain,
-  "local setmetatable = GLOBAL.setmetatable;\n" +
+updateFile(
+  resolve(__dirname, "./lib/modmain.lua"),
+  content =>
+    "local require = GLOBAL.require;\n" +
+    "local setmetatable = GLOBAL.setmetatable;\n" +
     "local unpack = GLOBAL.unpack;\n" +
-    readFileSync(modmain, "utf8").toString()
+    content.replace(new RegExp("scripts.omnikey.", "g"), "omnikey/")
+);
+
+updateFile(resolve(__dirname, "./lib/scripts/omnikey/cache.lua"), content =>
+  content.replace(new RegExp("GLOBAL.", "g"), "")
+);
+
+updateFile(resolve(__dirname, "./lib/scripts/omnikey/values.lua"), content =>
+  content.replace(new RegExp("GLOBAL.", "g"), "")
 );
--- src/modmain.ts
+++ src/modmain.ts
@@ -1,5 +1,17 @@
 /** @noSelfInFile **/
 
+import { getPrefabCopy } from "./scripts/omnikey/cache";
+import {
+  axeValue,
+  pickaxeValue,
+  weaponValue,
+  lightValue,
+  caneValue,
+  clothValue,
+  armorHeadValue,
+  armorBodyValue
+} from "./scripts/omnikey/values";
+
 const ACTION_DISTANCE = 40;
 
 class SingleThread {
@@ -16,6 +28,82 @@ class SingleThread {
   }
 }
 
+interface Config {
+  tags?: string[];
+  action: GLOBAL.Action;
+  filter?: (this: void, item: Prefabs.Item) => boolean | undefined;
+  recipes?: string[];
+  equip?: (this: void, item: PrefabCopy) => number;
+  faster?: boolean;
+  exclude?: string[];
+}
+
+const config = {
+  pick: {
+    action: GLOBAL.ACTIONS.PICK,
+    tags: ["pickable"],
+    exclude: ["flower"]
+  },
+  pickup: {
+    action: GLOBAL.ACTIONS.PICKUP,
+    tags: ["_inventoryitem"],
+    filter: (item: Prefabs.Item) =>
+      item.replica.inventoryitem && item.replica.inventoryitem.CanBePickedUp()
+  },
+  chop: {
+    action: GLOBAL.ACTIONS.CHOP,
+    tags: ["CHOP_workable"],
+    equip: axeValue,
+    recipes: ["goldenaxe", "axe"],
+    faster: true
+  },
+  mine: {
+    action: GLOBAL.ACTIONS.MINE,
+    tags: ["MINE_workable"],
+    equip: pickaxeValue,
+    recipes: ["goldenpickaxe", "pickaxe"],
+    faster: true
+  }
+};
+
+function main(pc: Prefabs.Player["components"]["playercontroller"]) {
+  const thread = new SingleThread();
+  const prevOnControl = pc.OnControl;
+  pc.OnControl = function(this, control, down) {
+    if (
+      control === GLOBAL.CONTROL_MOVE_UP ||
+      control === GLOBAL.CONTROL_MOVE_DOWN ||
+      control === GLOBAL.CONTROL_MOVE_LEFT ||
+      control === GLOBAL.CONTROL_MOVE_RIGHT
+    )
+      thread.stop();
+    prevOnControl.call(this, control, down);
+  };
+  for (const [key, data] of [
+    [GetModConfigData("MINE"), config.mine],
+    [GetModConfigData("CHOP"), config.chop],
+    [GetModConfigData("PICK"), config.pick],
+    [GetModConfigData("PICKUP"), config.pickup]
+  ]) {
+    GLOBAL.TheInput.AddKeyUpHandler(key, () => {
+      if (isReady())
+        thread.start(() => {
+          while (doThreadAction(data));
+        });
+    });
+  }
+  for (const [key, fn] of [
+    [GetModConfigData("WEAPON"), weaponValue],
+    [GetModConfigData("HELMET"), armorHeadValue],
+    [GetModConfigData("ARMOR"), armorBodyValue],
+    [GetModConfigData("LIGHT"), lightValue]
+  ]) {
+    GLOBAL.TheInput.AddKeyUpHandler(key, () => {
+      if (isReady()) ensureEquipped(fn, true);
+    });
+  }
+}
+
 function isReady() {
   return (
     !GLOBAL.IsPaused() &&
@@ -24,6 +112,16 @@ function isReady() {
     typeof GLOBAL.TheFrontEnd.GetActiveScreen().name === "string" &&
     GLOBAL.TheFrontEnd.GetActiveScreen().name === "HUD" &&
     GLOBAL.ThePlayer
+  );
+}
+
+function isBusy() {
+  const player = GLOBAL.ThePlayer;
+  return (
+    player.replica.builder.IsBusy() ||
+    player.components.playercontroller.IsDoingOrWorking() ||
+    !player.HasTag("idle") ||
+    player.HasTag("moving")
   );
 }
 
@@ -66,119 +164,11 @@ function makeRecipe(recipeName: string) {
   return false;
 }
 
-interface Config {
-  tags?: string[];
-  action: GLOBAL.Action;
-  filter?: (this: void, item: Prefabs.Item) => boolean | undefined;
-  recipes?: string[];
-  equip?: (this: void, item: PrefabCopy) => number;
-  faster?: boolean;
-  exclude?: string[];
-}
-
-const config = {
-  pick: {
-    action: GLOBAL.ACTIONS.PICK,
-    tags: ["pickable"],
-    exclude: ["flower"]
-  },
-  pickup: {
-    action: GLOBAL.ACTIONS.PICKUP,
-    tags: ["_inventoryitem"],
-    filter: (item: Prefabs.Item) =>
-      item.replica.inventoryitem && item.replica.inventoryitem.CanBePickedUp()
-  },
-  chop: {
-    action: GLOBAL.ACTIONS.CHOP,
-    tags: ["CHOP_workable"],
-    equip: (item: PrefabCopy) => toolValue(item, GLOBAL.ACTIONS.CHOP),
-    recipes: ["goldenaxe", "axe"],
-    faster: true
-  },
-  mine: {
-    action: GLOBAL.ACTIONS.MINE,
-    tags: ["MINE_workable"],
-    equip: (item: PrefabCopy) => toolValue(item, GLOBAL.ACTIONS.MINE),
-    recipes: ["goldenpickaxe", "pickaxe"],
-    faster: true
-  }
-};
-
-function isBusy() {
-  const player = GLOBAL.ThePlayer;
-  return (
-    player.replica.builder.IsBusy() ||
-    player.components.playercontroller.IsDoingOrWorking() ||
-    !player.HasTag("idle") ||
-    player.HasTag("moving")
-  );
-}
-
 function tryMakeRecipes(recipes: string[]): boolean {
   const index = recipes.findIndex(recipeName => canMakeRecipe(recipeName));
   if (index === -1) return false;
   makeRecipe(recipes[index]);
   return true;
-}
-
-function main(pc: Prefabs.Player["components"]["playercontroller"]) {
-  const thread = new SingleThread();
-  const prevOnControl = pc.OnControl;
-  pc.OnControl = function(this, control, down) {
-    if (
-      control === GLOBAL.CONTROL_MOVE_UP ||
-      control === GLOBAL.CONTROL_MOVE_DOWN ||
-      control === GLOBAL.CONTROL_MOVE_LEFT ||
-      control === GLOBAL.CONTROL_MOVE_RIGHT
-    )
-      thread.stop();
-    prevOnControl.call(this, control, down);
-  };
-  for (const [key, data] of [
-    [GetModConfigData("MINE"), config.mine],
-    [GetModConfigData("CHOP"), config.chop],
-    [GetModConfigData("PICK"), config.pick],
-    [GetModConfigData("PICKUP"), config.pickup]
-  ]) {
-    GLOBAL.TheInput.AddKeyUpHandler(key, () => {
-      if (isReady())
-        thread.start(() => {
-          while (doThreadAction(data));
-        });
-    });
-  }
-  for (const [key, fn] of [
-    [GetModConfigData("WEAPON"), valueByDamage],
-    [
-      GetModConfigData("HELMET"),
-      (item: PrefabCopy) => armorValue(item, GLOBAL.EQUIPSLOTS.HEAD)
-    ],
-    [
-      GetModConfigData("ARMOR"),
-      (item: PrefabCopy) => armorValue(item, GLOBAL.EQUIPSLOTS.BODY)
-    ],
-    [
-      GetModConfigData("LIGHT"),
-      (item: PrefabCopy) => {
-        const index = [
-          "gears_hat_goggles",
-          "molehat",
-          "bottlelantern",
-          "lantern",
-          "minerhat",
-          "tarlamp",
-          "lighter",
-          "torch"
-        ].indexOf(item.prefab);
-        if (index === -1) return 0;
-        return 1 / index;
-      }
-    ]
-  ]) {
-    GLOBAL.TheInput.AddKeyUpHandler(key, () => {
-      if (isReady()) ensureEquipped(fn, true);
-    });
-  }
 }
 
 function doThreadAction({
@@ -218,6 +208,10 @@ function doThreadAction({
   return true;
 }
 
+export interface PrefabCopy extends ReturnType<typeof getPrefabCopy> {
+  original: Prefab;
+}
+
 function ensureEquipped(fn: (item: PrefabCopy) => number, unequip = false) {
   const item = getBestItem(fn);
   if (!item) return false;
@@ -237,60 +231,6 @@ function ensureEquipped(fn: (item: PrefabCopy) => number, unequip = false) {
         clothValue(item, copy.components.equippable!.equipslot)
       ) || GLOBAL.ThePlayer.replica.inventory.UseItemFromInvTile(item);
   return true;
-}
-
-function copyPrefab(prefab: string) {
-  const isMasterSim = GLOBAL.TheWorld.ismastersim;
-  GLOBAL.TheWorld.ismastersim = true;
-  const copy = GLOBAL.SpawnPrefab(prefab);
-  const cache = {
-    prefab,
-    components: {
-      waterproofer: pick(copy.components.waterproofer, ["effectiveness"]),
-      insulator: pick(copy.components.insulator, ["insulation"]),
-      eater: pick(copy.components.eater, ["preferseating"]),
-      finiteuses: pick(copy.components.finiteuses, [
-        "current",
-        "total",
-        "consumption"
-      ]),
-      tool: pick(copy.components.tool, ["actions"]),
-      weapon: pick(copy.components.weapon, ["damage"]),
-      armor: pick(copy.components.armor, [
-        "absorb_percent",
-        "condition",
-        "maxcondition"
-      ]),
-      healer: pick(copy.components.healer, ["health"]),
-      equippable: pick(copy.components.equippable, [
-        "dapperness",
-        "equipslot",
-        "walkspeedmult"
-      ]),
-      edible: pick(copy.components.edible, [
-        "healthvalue",
-        "hungervalue",
-        "sanityvalue",
-        "foodtype"
-      ])
-    }
-  };
-  copy.Remove();
-  GLOBAL.TheWorld.ismastersim = isMasterSim;
-  return cache;
-}
-
-interface PrefabCopy extends ReturnType<typeof copyPrefab> {
-  original: Prefab;
-}
-
-const prefabCache: Record<string, ReturnType<typeof copyPrefab>> = {};
-
-function getPrefabCopy(prefab: string) {
-  if (prefabCache[prefab] === undefined) {
-    prefabCache[prefab] = copyPrefab(prefab);
-  }
-  return prefabCache[prefab];
 }
 
 function getBestItem(
@@ -361,100 +301,3 @@ function getCopy<T>(obj: T): T {
 }
 
 AddComponentPostInit("playercontroller", main as any);
-
-function clothValue(item: PrefabCopy, slot: any) {
-  return (
-    (canBeEquipped(item, slot) &&
-      valueByInsulation(item) + valueByDapperness(item) / 10) ||
-    0
-  );
-}
-
-function valueByDapperness(item: PrefabCopy) {
-  if (!item.components.equippable) return 0;
-  return item.components.equippable.dapperness || 0;
-}
-
-function valueByInsulation(item: PrefabCopy) {
-  if (!item.components.insulator) return 0;
-  return item.components.insulator.insulation || 0;
-}
-
-function canBeEquipped(item: PrefabCopy, slot: any) {
-  return (
-    item.components.equippable && item.components.equippable.equipslot === slot
-  );
-}
-
-function valueByDamage(item: PrefabCopy) {
-  return (
-    (item.components.weapon &&
-      item.components.weapon.damage * 100 + valueByUsage(item)) ||
-    0
-  );
-}
-
-function valueByUsage(item: PrefabCopy): number {
-  return (
-    (item.components.finiteuses !== undefined &&
-      item.components.finiteuses.total * 10 -
-        item.components.finiteuses.current) ||
-    0
-  );
-}
-
-function valueByConsumption(item: PrefabCopy, action: any): number {
-  return (
-    (item.components.finiteuses !== undefined &&
-      1 / item.components.finiteuses.consumption[action]) ||
-    100
-  );
-}
-
-function valueByArmor(item: PrefabCopy) {
-  return (
-    (item.components.armor &&
-      item.components.armor.absorb_percent * 100000 -
-        item.components.armor.condition) ||
-    0
-  );
-}
-
-function armorValue(item: PrefabCopy, slot: any) {
-  return (
-    (canBeEquipped(item, slot) &&
-      valueByArmor(item) + clothValue(item, slot)) ||
-    0
-  );
-}
-
-function toolValue(item: PrefabCopy, action: any) {
-  return (
-    (item.components.tool !== undefined &&
-      item.components.tool.actions[action] &&
-      valueByConsumption(item, action) * 1000 + valueByUsage(item)) ||
-    0
-  );
-}
-
-function caneValue(item: PrefabCopy) {
-  return (
-    (item.components.equippable &&
-      item.components.equippable.walkspeedmult &&
-      item.components.equippable.walkspeedmult > 1 &&
-      item.components.equippable.walkspeedmult) ||
-    0
-  );
-}
-
-function pick<T, K extends keyof T>(
-  t: T | undefined,
-  ks: K[]
-): Pick<T, K> | undefined {
-  if (t === undefined) return undefined;
-  const result = {} as Pick<T, K>;
-  for (const k of ks) {
-    result[k] = t[k];
-  }
-  return result;
-}
