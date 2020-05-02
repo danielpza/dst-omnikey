26281493f029c3029db79a7c52ae682ca8b591d6
Daniel Perez Alvarez <danielpza@protonmail.com>
2020-05-02 00:52:16 -0400

    improve build step

--- post-build.js
+++ post-build.js
@@ -2,12 +2,15 @@ const { readFileSync, writeFileSync } = require("fs");
 const { resolve } = require("path");
 
 const { version, description, homepage } = require("./package.json");
+const {
+  compilerOptions: { outDir },
+} = require("./tsconfig.json");
 
 function updateFile(file, updater) {
   writeFileSync(file, updater(readFileSync(file, "utf8").toString()));
 }
 
-updateFile(resolve(__dirname, "./lib/modinfo.lua"), content =>
+updateFile(resolve(__dirname, outDir, "modinfo.lua"), (content) =>
   content
     .replace("$VERSION", version)
     .replace("$DESCRIPTION", description)
@@ -15,18 +18,20 @@ updateFile(resolve(__dirname, "./lib/modinfo.lua"), content =>
 );
 
 updateFile(
-  resolve(__dirname, "./lib/modmain.lua"),
-  content =>
+  resolve(__dirname, outDir, "modmain.lua"),
+  (content) =>
     "local require = GLOBAL.require;\n" +
     "local setmetatable = GLOBAL.setmetatable;\n" +
     "local unpack = GLOBAL.unpack;\n" +
     content.replace(new RegExp("scripts.omnikey.", "g"), "omnikey/")
 );
 
-["cache", "utils", "values"].forEach(name => {
-  updateFile(resolve(__dirname, `./lib/scripts/omnikey/${name}.lua`), content =>
-    content
-      .replace(new RegExp("GLOBAL.", "g"), "")
-      .replace(new RegExp("scripts.omnikey.", "g"), "omnikey/")
+["cache", "utils", "values"].forEach((name) => {
+  updateFile(
+    resolve(__dirname, outDir, `scripts/omnikey/${name}.lua`),
+    (content) =>
+      content
+        .replace(new RegExp("GLOBAL.", "g"), "")
+        .replace(new RegExp("scripts.omnikey.", "g"), "omnikey/")
   );
 });
