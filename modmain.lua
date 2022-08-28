ce4d5b9ee9dfd492163c383ca7f498f4146677d0
Daniel Perez Alvarez <danielpza@protonmail.com>
2022-08-28 05:02:11 -0400

    some organization

--- modmain.lua
+++ modmain.lua
@@ -6,6 +6,25 @@ GLOBAL.require("debugkeys")
 ---@module 'scripts.omnikey.values'
 local values = require("omnikey/values")
 
+local EQUIP_KEYS = {
+	{
+		key = GetModConfigData("WEAPON"),
+		comparator = values.weaponValue,
+	},
+	{
+		key = GetModConfigData("LIGHT"),
+		comparator = values.lightValue,
+	},
+	{
+		key = GetModConfigData("ARMOR"),
+		comparator = values.armorValue(GLOBAL.EQUIPSLOTS.BODY),
+	},
+	{
+		key = GetModConfigData("HELMET"),
+		comparator = values.armorValue(GLOBAL.EQUIPSLOTS.HEAD),
+	},
+}
+
 ---@generic T
 ---@param prefab string
 ---@param fn fun(item: ds.entity): T
@@ -57,59 +76,46 @@ local function GetBestItemInInventory(comp)
 	return bestItem
 end
 
-AddComponentPostInit("playercontroller", function()
-	---@param fn fun(item: ds.entity): number
-	local function useMastersimPrefab(fn)
-		---@param item ds.entity
-		return function(item)
-			return WithMastersimPrefab(item.prefab, fn)
-		end
+---@param fn fun(item: ds.entity): number
+local function useMastersimPrefab(fn)
+	---@param item ds.entity
+	return function(item)
+		return WithMastersimPrefab(item.prefab, fn)
 	end
-	local keys = {
-		{
-			key = GetModConfigData("WEAPON"),
-			comparator = useMastersimPrefab(values.weaponValue),
-		},
-		{
-			key = GetModConfigData("LIGHT"),
-			comparator = useMastersimPrefab(values.lightValue),
-		},
-		{
-			key = GetModConfigData("ARMOR"),
-			comparator = useMastersimPrefab(values.armorValue(GLOBAL.EQUIPSLOTS.BODY)),
-		},
-		{
-			key = GetModConfigData("HELMET"),
-			comparator = useMastersimPrefab(values.armorValue(GLOBAL.EQUIPSLOTS.HEAD)),
-		},
-	}
+end
 
-	for _, action in ipairs(keys) do
-		GLOBAL.TheInput:AddKeyUpHandler(action.key, function()
-			local item = GetBestItemInInventory(action.comparator)
-			if item == nil or not item.replica.equippable then
-				return
-			end
-			if item.replica.equippable:IsEquipped() then
-				local equipslot = item.replica.equippable:EquipSlot()
-				if equipslot == GLOBAL.EQUIPSLOTS.HANDS then
-					local cane = GetBestItemInInventory(useMastersimPrefab(values.caneValue))
-					if cane then
-						GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(cane)
-					else
-						GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(item)
-					end
-				else
-					local clothEquip = GetBestItemInInventory(useMastersimPrefab(values.clothValue(equipslot)))
-					if clothEquip then
-						GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(clothEquip)
-					else
-						GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(item)
-					end
-				end
+---@param comparator fun(item: ds.entity): number|nil
+local function EquipUnequipItem(comparator)
+	local item = GetBestItemInInventory(useMastersimPrefab(comparator))
+	if item == nil or not item.replica.equippable then
+		return
+	end
+	if item.replica.equippable:IsEquipped() then
+		local equipslot = item.replica.equippable:EquipSlot()
+		if equipslot == GLOBAL.EQUIPSLOTS.HANDS then
+			local cane = GetBestItemInInventory(useMastersimPrefab(values.caneValue))
+			if cane then
+				GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(cane)
 			else
 				GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(item)
 			end
+		else
+			local clothEquip = GetBestItemInInventory(useMastersimPrefab(values.clothValue(equipslot)))
+			if clothEquip then
+				GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(clothEquip)
+			else
+				GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(item)
+			end
+		end
+	else
+		GLOBAL.ThePlayer.replica.inventory:UseItemFromInvTile(item)
+	end
+end
+
+AddComponentPostInit("playercontroller", function()
+	for _, action in ipairs(EQUIP_KEYS) do
+		GLOBAL.TheInput:AddKeyUpHandler(action.key, function()
+			EquipUnequipItem(action.comparator)
 		end)
 	end
 end)
