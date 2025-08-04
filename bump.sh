0190dd34fc4c15bc7277b9b7f2c62b2de053fa76
Daniel Perez Alvarez <danielpza@protonmail.com>
2025-08-03 19:11:40 -0700

    chore: add bump.sh

---
+++ bump.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+next_version=$(git cliff --bumped-version)
+sed "s/version = \".*\..*\..*\"/version = \"${next_version:1}\"/" modinfo.lua -i
+
+git-cliff --bump -o CHANGELOG.md
+
+git add modinfo.lua CHANGELOG.md
+
+git commit -m "chore(release): ${next_version:1}"
+git tag ${next_version}
